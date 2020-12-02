388cdab5853725c0e8129b6b1f98bd1c48a47117
lflorj00 <lflorj00@estudiantes.unileon.es>
2020-12-02 18:28:34 +0100

    Añadido el frontend de ElectroHits

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -56,24 +56,25 @@
         </v-layout>
       </v-navigation-drawer>
 
-      <v-carousel
-        cycle
-        height="300"
-        hide-delimiters
-        show-arrows-on-hover
-        alingn="center"
-        justify="center"
-      >
-        <v-carousel-item
-          v-for="(slide, i) in slides"
-          :key="i"
-          :src="slides.src"
-          transition="fade-transition"
-          :position="slide.position"
+      <v-content>
+        <v-carousel
+          cycle
+          height="300"
+          hide-delimiters
+          show-arrows-on-hover
+          alingn="center"
+          justify="center"
         >
-          <v-row class="fill-height" align="center" justify="center"> </v-row>
-        </v-carousel-item>
-      </v-carousel>
+          <v-carousel-item
+            v-for="(slide, i) in slides"
+            :key="i"
+            :src="slide.src"
+            reverse-transition="fade-transition"
+            transition="fade-transition"
+          >
+          </v-carousel-item>
+        </v-carousel>
+      </v-content>
 
       <v-container>
         <v-layout mt-4 column align-center wrap>
@@ -84,14 +85,17 @@
 
             <v-btn @click="alb" color="green" dark outlined>Álbumes</v-btn>
 
-            <v-btn @click="tulbi" color="orange" dark outlined>Recomendación</v-btn>
+            <v-btn @click="tulbi" color="orange" dark outlined
+              >Recomendación</v-btn
+            >
           </v-flex>
         </v-layout>
       </v-container>
 
       <v-card class="mx-auto" v-if="tultul">
         <v-card-title
-          >¿Que subgénero de música electrónica quieres escuchar?</v-card-title>
+          >¿Que subgénero de música electrónica quieres escuchar?</v-card-title
+        >
         <v-card-text>
           <v-row justify="space-around">
             <v-radio-group v-model="row" row>
@@ -203,8 +207,6 @@
               >
                 <v-card-title>{{ item.name }}</v-card-title>
                 <v-card-subtitle>
-                  Mood: {{ item.mood }}
-                  <br />
                   Grupo: {{ item.group }}
                   <br />
                   Duración: {{ item.duration }}
@@ -287,15 +289,12 @@
 </template>
 
 <script>
-import App from "../App.vue";
 export default {
   name: "Home",
   data: () => ({
-    i: false,
     array: [],
     yarra: [],
     pras: "0",
-    row: "Melancolia",
     buti2: false,
     buti3: false,
     buti4: false,
@@ -316,18 +315,16 @@ export default {
     return: {
       slides: [
         {
-          src: "https://i.ytimg.com/vi/WKuaujIHBT4/maxresdefault.jpg",
+          src: "./src/assets/imagen1.jpg"
         },
         {
-          src:
-            "https://i2.wp.com/www.knobstudio.com.mx/wp-content/uploads/2019/05/UMF-Bayfront.jpg?fit=2048%2C1152&ssl=1",
+          src: "./src/assets/imagen2.jpg"
         },
         {
-          src: "https://wallpapercave.com/wp/wp3770767.jpg",
+          src: "./src/assets/imagen3.jpg"
         },
         {
-          src:
-            "https://betronicmusic.com/wp-content/uploads/2018/08/ultra-music-festival-video-youredm.jpg",
+          src: "./src/assets/imagen4.jpg"
         },
       ],
     },
