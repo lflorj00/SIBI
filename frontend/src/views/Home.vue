b1efa0f7fda13f99ab68943b7e393700179b68fe
lflorj00 <lflorj00@estudiantes.unileon.es>
2020-12-04 01:32:43 +0100

    Añadido el frontend de ElectroHits

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,10 +1,6 @@
 <template>
-  <v-app>
+  <v-app dark>
     <v-card color="grey darken-4" dark height="50px" tile>
-      <v-content>
-        <router-view></router-view>
-      </v-content>
-
       <v-btn
         v-scroll="onScroll"
         v-show="fab"
@@ -56,7 +52,7 @@
         </v-layout>
       </v-navigation-drawer>
 
-      <v-content>
+      <v-main>
         <v-carousel
           cycle
           height="300"
@@ -68,233 +64,240 @@
           <v-carousel-item
             v-for="(slide, i) in slides"
             :key="i"
-            :src="slide.src"
-            reverse-transition="fade-transition"
+            :src="slides.src"
             transition="fade-transition"
+            :position="slide.position"
           >
+            <v-row class="fill-height" align="center" justify="center"> </v-row>
           </v-carousel-item>
         </v-carousel>
-      </v-content>
 
-      <v-container>
-        <v-layout mt-4 column align-center wrap>
-          <v-flex>
-            <v-btn @click="can" color="blue" dark outlined>Canciones</v-btn>
+        <v-container>
+          <v-layout mt-4 column align-center wrap>
+            <v-flex>
+              <v-btn @click="can" color="blue" dark outlined>Canciones</v-btn>
 
-            <v-btn @click="art" color="red" dark outlined>Artistas</v-btn>
+              <v-btn @click="art" color="red" dark outlined>Artistas</v-btn>
 
-            <v-btn @click="alb" color="green" dark outlined>Álbumes</v-btn>
+              <v-btn @click="alb" color="green" dark outlined>Álbumes</v-btn>
 
-            <v-btn @click="tulbi" color="orange" dark outlined
-              >Recomendación</v-btn
-            >
-          </v-flex>
-        </v-layout>
-      </v-container>
-
-      <v-card class="mx-auto" v-if="tultul">
-        <v-card-title
-          >¿Que subgénero de música electrónica quieres escuchar?</v-card-title
-        >
-        <v-card-text>
-          <v-row justify="space-around">
-            <v-radio-group v-model="row" row>
-              <v-radio label="House" value="House"></v-radio>
-              <v-radio label="Deep House" value="Deep House"></v-radio>
-              <v-radio
-                label="Trance Progresivo"
-                value="Trance Progresivo"
-              ></v-radio>
-              <v-radio label="Dubpstep" value="Dubpstep"></v-radio>
-              <v-radio label="Dance" value="Dance"></v-radio>
-              <v-radio label="Electro House" value="Electro House"></v-radio>
-              <v-radio
-                label="House Progrsivo"
-                value="House Progresivo"
-              ></v-radio>
-            </v-radio-group>
-          </v-row>
-          <v-container>
-            <v-layout mt-4 column align-center wrap>
-              <v-btn @click="real" color="orange" outlined dark
-                >Aleatorio</v-btn
+              <v-btn @click="tulbi" color="orange" dark outlined
+                >Recomendación</v-btn
               >
-              <v-btn @click="reco" color="orange" outlined dark
-                >Recomendar</v-btn
-              >
+            </v-flex>
+          </v-layout>
+        </v-container>
+
+        <v-card class="mx-auto" v-if="tultul">
+          <v-card-title
+            >¿Que subgénero de música electrónica quieres
+            escuchar?</v-card-title
+          >
+          <v-card-text>
+            <v-row justify="space-around">
+              <v-radio-group v-model="row" row>
+                <v-radio label="House" value="House"></v-radio>
+                <v-radio label="Deep House" value="Deep House"></v-radio>
+                <v-radio
+                  label="Trance Progresivo"
+                  value="Trance Progresivo"
+                ></v-radio>
+                <v-radio label="Dubpstep" value="Dubpstep"></v-radio>
+                <v-radio label="Dance" value="Dance"></v-radio>
+                <v-radio label="Electro House" value="Electro House"></v-radio>
+                <v-radio
+                  label="House Progrsivo"
+                  value="House Progresivo"
+                ></v-radio>
+              </v-radio-group>
+            </v-row>
+            <v-container>
+              <v-layout mt-4 column align-center wrap>
+                <v-btn @click="real" color="orange" outlined dark
+                  >Aleatorio</v-btn
+                >
+                <v-btn @click="reco" color="orange" outlined dark
+                  >Recomendar</v-btn
+                >
+              </v-layout>
+            </v-container>
+          </v-card-text>
+        </v-card>
+
+        <v-content v-if="this.buti2">
+          <v-container grid-list-md text-xs-center fluid pa-12>
+            <v-text-field
+              label="Canción"
+              v-model="search"
+              single-line
+            ></v-text-field>
+            <v-layout row wrap fill-height fill-width justify-center>
+              <v-flex v-for="(item, index) in array" v-bind:key="index">
+                <v-card
+                  elevation="18"
+                  style="background: #3A1C71;
+    background: -webkit-linear-gradient(to bottom right, #636363, #808080, #939393);
+    background: linear-gradient(to bottom right,  #636363, #808080, #939393);"
+                  dark
+                >
+                  <v-card-title>{{ item.name }}</v-card-title>
+                  <v-card-subtitle>
+                    Nombre: {{ item.name }}
+                    <br />
+                    Lanzamiento: {{ item.released }}
+                    <br />
+                    Duración: {{ item.duration }}
+                    <br />
+                    Album: {{ item.album }}
+                    <br />
+                    Número de pista: {{ item.trackNumber }}
+                    <br />
+                    Grupo: {{ item.group }}
+                  </v-card-subtitle>
+                </v-card>
+              </v-flex>
             </v-layout>
           </v-container>
-        </v-card-text>
-      </v-card>
+        </v-content>
 
-      <v-content v-if="this.buti2">
-        <v-container grid-list-md text-xs-center fluid pa-12>
-          <v-text-field
-            label="Canción"
-            v-model="search"
-            single-line
-          ></v-text-field>
-          <v-layout row wrap fill-height fill-width justify-center>
-            <v-flex v-for="(item, index) in array" v-bind:key="index">
-              <v-card
-                elevation="18"
-                style="background: #3A1C71;
+        <v-content v-if="this.buti6">
+          <v-container grid-list-md text-xs-center fluid pa-12>
+            <v-layout row wrap fill-height fill-width justify-center>
+              <v-flex v-for="(item, index) in array" v-bind:key="index">
+                <v-card
+                  elevation="18"
+                  style="background: #3A1C71;
     background: -webkit-linear-gradient(to bottom right, #636363, #808080, #939393);
     background: linear-gradient(to bottom right,  #636363, #808080, #939393);"
-                dark
-              >
-                <v-card-title>{{ item.name }}</v-card-title>
-                <v-card-subtitle>
-                  Nombre: {{ item.name }}
-                  <br />
-                  Lanzamiento: {{ item.released }}
-                  <br />
-                  Duración: {{ item.duration }}
-                  <br />
-                  Album: {{ item.album }}
-                  <br />
-                  Número de pista: {{ item.trackNumber }}
-                  <br />
-                  Grupo: {{ item.group }}
-                </v-card-subtitle>
-              </v-card>
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </v-content>
+                  dark
+                  min-width="200"
+                >
+                  <v-card-title>{{ item.name }}</v-card-title>
+                  <v-card-subtitle>
+                    Año: {{ item.released }}
+                    <br />
+                    Duración: {{ item.duration }}
+                    <br />
+                    Número de pista: {{ item.trackNumber }}
+                    <br />
+                    Album: {{ item.album }}
+                    <br />
+                    Grupo: {{ item.group }}
+                  </v-card-subtitle>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-content>
 
-      <v-content v-if="this.buti6">
-        <v-container grid-list-md text-xs-center fluid pa-12>
-          <v-layout row wrap fill-height fill-width justify-center>
-            <v-flex v-for="(item, index) in array" v-bind:key="index">
-              <v-card
-                elevation="18"
-                style="background: #3A1C71;
+        <v-content v-if="this.buti5">
+          <v-container grid-list-md text-xs-center fluid pa-12>
+            <v-layout row wrap fill-height fill-width justify-center>
+              <v-flex v-for="(item, index) in yarra" v-bind:key="index">
+                <v-card
+                  elevation="18"
+                  style="background: #3A1C71;
     background: -webkit-linear-gradient(to bottom right, #636363, #808080, #939393);
     background: linear-gradient(to bottom right,  #636363, #808080, #939393);"
-                dark
-                min-width="200"
-              >
-                <v-card-title>{{ item.name }}</v-card-title>
-                <v-card-subtitle>
-                  Año: {{ item.released }}
-                  <br />
-                  Duración: {{ item.duration }}
-                  <br />
-                  Número de pista: {{ item.trackNumber }}
-                  <br />
-                  Album: {{ item.album }}
-                  <br />
-                  Grupo: {{ item.group }}
-                </v-card-subtitle>
-              </v-card>
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </v-content>
+                  dark
+                  min-width="200"
+                >
+                  <v-card-title>{{ item.name }}</v-card-title>
+                  <v-card-subtitle>
+                    Mood: {{ item.mood }}
+                    <br />
+                    Grupo: {{ item.group }}
+                    <br />
+                    Duración: {{ item.duration }}
+                    <br />
+                    Número de pista: {{ item.trackNumber }}
+                    <br />
+                    Album: {{ item.album }}
+                    <br />
+                    Año: {{ item.released }}
+                  </v-card-subtitle>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-content>
 
-      <v-content v-if="this.buti5">
-        <v-container grid-list-md text-xs-center fluid pa-12>
-          <v-layout row wrap fill-height fill-width justify-center>
-            <v-flex v-for="(item, index) in yarra" v-bind:key="index">
-              <v-card
-                elevation="18"
-                style="background: #3A1C71;
+        <v-content v-if="this.buti3">
+          <v-container grid-list-md text-xs-center fluid pa-12>
+            <v-layout row wrap fill-height fill-width justify-center>
+              <v-text-field
+                label="Artistas"
+                v-model="search"
+                single-line
+              ></v-text-field>
+              <v-flex v-for="(item, index) in array" v-bind:key="index">
+                <v-card
+                  elevation="18"
+                  style="background: #3A1C71;
     background: -webkit-linear-gradient(to bottom right, #636363, #808080, #939393);
     background: linear-gradient(to bottom right,  #636363, #808080, #939393);"
-                dark
-                min-width="200"
-              >
-                <v-card-title>{{ item.name }}</v-card-title>
-                <v-card-subtitle>
-                  Grupo: {{ item.group }}
-                  <br />
-                  Duración: {{ item.duration }}
-                  <br />
-                  Número de pista: {{ item.trackNumber }}
-                  <br />
-                  Album: {{ item.album }}
-                  <br />
-                  Año: {{ item.released }}
-                </v-card-subtitle>
-              </v-card>
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </v-content>
+                  dark
+                >
+                  <v-card-title>{{ item.name }}</v-card-title>
+                  <v-card-subtitle>
+                    Nombre: {{ item.name }}
+                    <br />
+                    Lugar de nacimiento: {{ item.from }}
+                    <br />
+                    Año de nacimiento: {{ item.born }}
+                    <br />
+                    Subgénero: {{ item.subgender }}
+                  </v-card-subtitle>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-content>
 
-      <v-content v-if="this.buti3">
-        <v-container grid-list-md text-xs-center fluid pa-12>
-          <v-layout row wrap fill-height fill-width justify-center>
-            <v-text-field
-              label="Artistas"
-              v-model="search"
-              single-line
-            ></v-text-field>
-            <v-flex v-for="(item, index) in array" v-bind:key="index">
-              <v-card
-                elevation="18"
-                style="background: #3A1C71;
+        <v-content v-if="this.buti4">
+          <v-container grid-list-md text-xs-center fluid pa-12>
+            <v-layout row wrap fill-height fill-width justify-center>
+              <v-text-field
+                label="Álbumes"
+                v-model="search"
+                single-line
+              ></v-text-field>
+              <v-flex v-for="(item, index) in array" v-bind:key="index">
+                <v-card
+                  elevation="18"
+                  style="background: #3A1C71;
     background: -webkit-linear-gradient(to bottom right, #636363, #808080, #939393);
     background: linear-gradient(to bottom right,  #636363, #808080, #939393);"
-                dark
-              >
-                <v-card-title>{{ item.name }}</v-card-title>
-                <v-card-subtitle>
-                  Nombre: {{ item.name }}
-                  <br />
-                  Lugar de nacimiento: {{ item.from }}
-                  <br />
-                  Año de nacimiento: {{ item.born }}
-                  <br />
-                  Subgénero: {{ item.subgender }}
-                </v-card-subtitle>
-              </v-card>
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </v-content>
-
-      <v-content v-if="this.buti4">
-        <v-container grid-list-md text-xs-center fluid pa-12>
-          <v-layout row wrap fill-height fill-width justify-center>
-            <v-text-field
-              label="Álbumes"
-              v-model="search"
-              single-line
-            ></v-text-field>
-            <v-flex v-for="(item, index) in array" v-bind:key="index">
-              <v-card
-                elevation="18"
-                style="background: #3A1C71;
-    background: -webkit-linear-gradient(to bottom right, #636363, #808080, #939393);
-    background: linear-gradient(to bottom right,  #636363, #808080, #939393);"
-                dark
-              >
-                <v-card-title>{{ item.name }}</v-card-title>
-                <v-card-subtitle>
-                  Nombre: {{ item.name }}
-                  <br />
-                  Lanzamiento: {{ item.released }}
-                  <br />
-                  Grupo: {{ item.group }}
-                </v-card-subtitle>
-              </v-card>
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </v-content>
+                  dark
+                >
+                  <v-card-title>{{ item.name }}</v-card-title>
+                  <v-card-subtitle>
+                    Nombre: {{ item.name }}
+                    <br />
+                    Lanzamiento: {{ item.released }}
+                    <br />
+                    Grupo: {{ item.group }}
+                  </v-card-subtitle>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-content>
+      </v-main>
     </v-card>
   </v-app>
 </template>
 
 <script>
+import App from "../App.vue";
 export default {
   name: "Home",
   data: () => ({
+    i: false,
     array: [],
     yarra: [],
     pras: "0",
+    row: "Melancolia",
     buti2: false,
     buti3: false,
     buti4: false,
@@ -305,7 +308,6 @@ export default {
     search: "",
     drawer: false,
     group: null,
-
     watch: {
       group() {
         this.drawer = false;
@@ -315,21 +317,22 @@ export default {
     return: {
       slides: [
         {
-          src: "./src/assets/imagen1.jpg"
+          src: "https://i.ytimg.com/vi/WKuaujIHBT4/maxresdefault.jpg",
         },
         {
-          src: "./src/assets/imagen2.jpg"
+          src:
+            "https://i2.wp.com/www.knobstudio.com.mx/wp-content/uploads/2019/05/UMF-Bayfront.jpg?fit=2048%2C1152&ssl=1",
         },
         {
-          src: "./src/assets/imagen3.jpg"
+          src: "https://wallpapercave.com/wp/wp3770767.jpg",
         },
         {
-          src: "./src/assets/imagen4.jpg"
+          src:
+            "https://betronicmusic.com/wp-content/uploads/2018/08/ultra-music-festival-video-youredm.jpg",
         },
       ],
     },
   }),
-
   methods: {
     tulbi() {
       this.tultul = true;
@@ -354,7 +357,6 @@ export default {
         }
       });
     },
-
     reco() {
       this.buti2 = false;
       this.buti3 = false;
